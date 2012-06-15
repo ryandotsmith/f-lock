4dd3eb97ab0e0a74a82076b7d2b8a85c678aa508
♠ ace hacker <ryan@heroku.com>
2012-06-15 13:01:19 -0700

    ignore lock file

--- init.sql
+++ init.sql
@@ -8,8 +8,8 @@ insert into zones(name) values('shushud-partitioned.net.');
 
 insert into endpoints(name, uri) values(
        'shushu.herokuapp.com',
-       'https://1:123@shushu.herokuapp.com/heartbeat');
+       'https://1:3796@shushu.herokuapp.com/heartbeat');
 
 insert into endpoints(name, uri) values(
        'shushu.staging.herokuappdev.com',
-       'https://1:123@shushu.staging.herokuappdev.com/heartbeat');
+       'https://1:3796@shushu.staging.herokuappdev.com/heartbeat');
